4a62089b977296451c60c85856e053cf43b8031c
Raimo Tuisku <raimo.tuisku@gmail.com>
2014-06-30 18:35:47 +0300

    Add a title and move createLabel to leap.widgets.js.

--- javascripts/leap.widgets.js
+++ javascripts/leap.widgets.js
@@ -3,6 +3,7 @@
   var BUTTON_COLOR_ACTIVE = 0x81d41d;
 
   window.LeapWidgets = function (scene) {
+    this.scene = scene;
     this.buttons = [];
   }
 
@@ -18,9 +19,9 @@
     button.position.copy(button.originalposition);
     button.receiveShadow = true;
     button.castShadow = true;
-    scene.add(button);
+    this.scene.add(button);
 
-    button.add(createLabel(text, new THREE.Vector3(0, 0, dimensions.z/2+1), 14, 0xffffff));
+    this.createLabel(text, new THREE.Vector3(0, 0, dimensions.z/2+1), 14, 0xffffff, button);
 
     button.slider = new Physijs.SliderConstraint(
       button,
@@ -28,12 +29,47 @@
       new THREE.Vector3(0, 0, -200),
       new THREE.Vector3(0, Math.sqrt(2), Math.sqrt(2))
     );
-    scene.addConstraint(button.slider);
+    this.scene.addConstraint(button.slider);
     button.slider.setLimits(-150, 0, 0, 0);
   //      button.slider.setRestitution(0, 0);
     this.buttons.push(button);
     return button;
+  };
+
+  LeapWidgets.prototype.createLabel = function(text, position, size, color, addTo) {
+    var hexpadding = "#000000";
+    var canvas = document.createElement("canvas");
+
+    var context = canvas.getContext("2d");
+    context.font = size + "pt Helvetica";
+
+    var textWidth = context.measureText(text).width;
+
+    canvas.width = textWidth;
+    canvas.height = size + 2;
+    context = canvas.getContext("2d");
+    context.font = size + "pt Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.fillStyle = hexpadding.substring(0, hexpadding.length - color.toString(16).length) + color.toString(16);
+    context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    var material = new THREE.MeshBasicMaterial({
+      map : texture,
+      transparent: true
+    });
+
+    var mesh = new THREE.Mesh(new THREE.PlaneGeometry(canvas.width, canvas.height), material);
+    mesh.doubleSided = true;
+    mesh.position.copy(position);
+
+    (addTo || this.scene).add(mesh);
+    return mesh;
   }
+
 
 
   LeapWidgets.prototype.update = function() {
