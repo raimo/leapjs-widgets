0d7c216f3c9a3679188f2b985a9a15b4d690a099
Raimo Tuisku <raimo.tuisku@gmail.com>
2014-07-01 15:50:00 +0300

    Add knob switch.

--- javascripts/leap.widgets.js
+++ javascripts/leap.widgets.js
@@ -5,6 +5,7 @@
   window.LeapWidgets = function (scene) {
     this.scene = scene;
     this.buttons = [];
+    this.switches = [];
   }
 
   LeapWidgets.prototype.createButton = function(text, position, dimensions) {
@@ -34,6 +35,44 @@
   //      button.slider.setRestitution(0, 0);
     this.buttons.push(button);
     return button;
+  };
+
+  LeapWidgets.prototype.createSwitch = function(text, position, radius, height) {
+    var stick = new Physijs.BoxMesh(
+      new THREE.CylinderGeometry(radius, radius, height),
+      Physijs.createMaterial(new THREE.MeshPhongMaterial({
+        color: BUTTON_COLOR
+      }), 1, 0.9),
+      1
+    );
+    stick.originalposition = position;
+    stick.position.copy(stick.originalposition);
+    stick.receiveShadow = true;
+    stick.castShadow = true;
+    this.scene.add(stick);
+
+    this.createLabel(text, new THREE.Vector3(0, 0, radius), 14, 0xffffff);
+
+    var knob = new Physijs.SphereMesh(
+        new THREE.SphereGeometry(32, 27),
+        Physijs.createMaterial(new THREE.MeshPhongMaterial({color: 0xcc1122}), 0, 0),
+        100
+    );
+    knob.position.y = height/2;
+    knob.castShadow = true;
+    stick.add(knob);
+
+
+    var point = new Physijs.PointConstraint(
+      stick,
+      null,
+      new THREE.Vector3(stick.position.x, stick.position.y - height/2, stick.position.z)
+    );
+    this.scene.addConstraint(point);
+  //  stick.slider.setLimits(-150, -150, 0, 0);
+  //  stick.slider.setRestitution(0, 0);
+    this.switches.push(stick);
+    return stick;
   };
 
   LeapWidgets.prototype.createLabel = function(text, position, size, color, addTo) {
@@ -76,6 +115,10 @@
     this.buttons.forEach(function(button) {
       button.setLinearVelocity(new THREE.Vector3().copy(button.originalposition).sub(button.position).multiplyScalar(16));
       button.material.color.setHex(button.position.z+2 < button.originalposition.z ? BUTTON_COLOR_ACTIVE : BUTTON_COLOR);
+    });
+    this.switches.forEach(function(stick) {
+      stick.setLinearVelocity(new THREE.Vector3(0,1000,0));
+      stick.setAngularVelocity(new THREE.Vector3(0,0,0));
     });
   };
 })();
